a1371c904c222ac04155ea5dc93a4fae381accc0
iiNxM3 <121332546+Natakorn1234@users.noreply.github.com>
2026-03-16 10:59:54 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,45 +1,25 @@
 local universeId = game.GameId
-local placeId = game.PlaceId
 
 print("UniverseId:", universeId)
-print("PlaceId:", placeId)
 
 local games = {
 
-    [1202096104] = { -- Driving Empire
-        places = {
-
-            [3351674303] = "https://raw.githubusercontent.com/YOURNAME/hub/main/scriptLobby.lua",
-
-        }
-    }
+    [1202096104] = "https://raw.githubusercontent.com/YOURNAME/hub/main/games/drivingEmpire.lua"
 
 }
 
-local gameData = games[universeId]
+local scriptUrl = games[universeId]
 
-if gameData then
+if not scriptUrl then
+    return warn("Game not supported:", universeId)
+end
 
-    local scriptUrl = gameData.places[placeId]
+print("Loading game script...")
 
-    if scriptUrl then
+local success, err = pcall(function()
+    loadstring(game:HttpGet(scriptUrl))()
+end)
 
-        print("Loading script...")
-
-        local success, err = pcall(function()
-            task.spawn(function()
-                loadstring(game:HttpGet(scriptUrl))()
-            end)
-        end)
-
-        if not success then
-            warn("Script failed:", err)
-        end
-
-    else
-        warn("Place not supported:", placeId)
-    end
-
-else
-    warn("Game not supported:", universeId)
+if not success then
+    warn("Script failed:", err)
 end
